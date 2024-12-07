23599a4282f53fefed60116aa72987807babf40f
Fresh Prince <mario.n.slavov2004@gmail.com>
2024-12-07 12:34:47 +0200

    Add Class 07122004 and a Perfect Balanced tree

---
+++ cmake-build-debug/CMakeFiles/PurfectBalancedTree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PurfectBalancedTree.dir/Class07122024/PurfectBalancedTree.cpp.obj"
+  "CMakeFiles/PurfectBalancedTree.dir/Class07122024/PurfectBalancedTree.cpp.obj.d"
+  "PurfectBalancedTree.exe"
+  "PurfectBalancedTree.exe.manifest"
+  "PurfectBalancedTree.pdb"
+  "libPurfectBalancedTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PurfectBalancedTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
